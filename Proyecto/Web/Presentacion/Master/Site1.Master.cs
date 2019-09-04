649fbc927213f5327e24febb43928231ab4ba195
Valentina Páez <svpaez4@misena.edu.co>
2019-09-04 17:23:29 -0500

    Cambio diseño master, validacion de usuario

--- Proyecto/Web/Presentacion/Master/Site1.Master.cs
+++ Proyecto/Web/Presentacion/Master/Site1.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,55 @@ namespace Web
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        int IdUsuario;
+
+        ServicioUsuario.WebService1SoapClient ServicioU = new ServicioUsuario.WebService1SoapClient();
+        ServicioUsuario.clUsuario objUsu = new ServicioUsuario.clUsuario();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            mtdVerificarUsuario();
+        }
 
+        protected void mtdVerificarUsuario()
+        {
+            try
+            {
+                IdUsuario = int.Parse(Application["IdUsuario"].ToString());
+            }
+            catch (Exception e)
+            {
+                IdUsuario = 0;
+            }
+
+            if (IdUsuario>0)
+            {
+                objUsu.IdUsuario = IdUsuario;
+                DataSet dsUsuario = new DataSet();
+                dsUsuario = ServicioU.mtdBuscar(objUsu);
+
+                Nombre.InnerText = dsUsuario.Tables["tblDatos"].Rows[0]["Nombre"].ToString() + " " + dsUsuario.Tables["tblDatos"].Rows[0]["Apellido"].ToString();
+                Nombre1.InnerText = dsUsuario.Tables["tblDatos"].Rows[0]["Nombre"].ToString() + " " + dsUsuario.Tables["tblDatos"].Rows[0]["Apellido"].ToString();
+                Rol.InnerText = dsUsuario.Tables["tblDatos"].Rows[0]["Rol"].ToString();
+
+                MenuUsuario.Attributes["style"] = "visibility:visible";
+                inS.Attributes["style"] = "visibility:hidden";
+                alarm.Attributes["style"] = "visibility:visible";
+                terr.Attributes["style"] = "visibility:visible";
+                menEta1.Attributes["style"] = "visibility:visible";
+                menInsu1.Attributes["style"] = "visibility:visible";
+                menForo1.Attributes["style"] = "visibility:visible";
+            }
+            else
+            {
+                inS.Attributes["style"] = "visibility:visible";
+                MenuUsuario.Attributes["style"] = "visibility:hidden";
+                alarm.Attributes["style"] = "visibility:hidden";
+                terr.Attributes["style"] = "visibility:hidden";
+                menEta1.Attributes["style"] = "visibility:hidden";
+                menInsu1.Attributes["style"] = "visibility:hidden";
+                menForo1.Attributes["style"] = "visibility:hidden";
+            }
         }
     }
 }
--- Proyecto/Web/Presentacion/Master/Site1.Master.designer.cs
+++ Proyecto/Web/Presentacion/Master/Site1.Master.designer.cs
@@ -22,6 +22,96 @@ namespace Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// Control terr.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl terr;
+        
+        /// <summary>
+        /// Control menEta1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menEta1;
+        
+        /// <summary>
+        /// Control menInsu1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menInsu1;
+        
+        /// <summary>
+        /// Control menForo1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menForo1;
+        
+        /// <summary>
+        /// Control alarm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl alarm;
+        
+        /// <summary>
+        /// Control inS.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl inS;
+        
+        /// <summary>
+        /// Control MenuUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl MenuUsuario;
+        
+        /// <summary>
+        /// Control Nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor Nombre;
+        
+        /// <summary>
+        /// Control Nombre1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor Nombre1;
+        
+        /// <summary>
+        /// Control Rol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Rol;
+        
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
